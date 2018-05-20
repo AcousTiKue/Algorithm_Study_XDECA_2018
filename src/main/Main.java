a4a7582eb685612ef15cab6b987b02e0177db2bc
AcousTiKue <acous_tyler6@naver.com>
2018-05-20 07:02:26 +0000

    '18.05.20' Latest commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,15 @@
+/*
+	 ** 
+	 ** Theme		|	[XDeca] Algorithm Study 
+	 ** Author		|	KTMO-Cell S/W Supporter(~'19), SukJoon Oh
+	 ** Environment	|	Windows 10 Professional, Eclipse-Oxygen, Java JDK 10.0.1
+	 **				|	Java
+	 ** Change Log	|	First written '18.05.17.
+	 ** File Info	|	Main.java
+	 **				|   For the execution on Goorm IDE.
+	 ** 
+ * */
+
 package test;
 
 public class Main {
